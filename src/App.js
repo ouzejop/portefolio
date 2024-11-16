feadd23cc7ad720c73bfa1a7340268c9f4c283d7
ouzejop <diopousmane779@gmail.com>
2024-11-16 21:36:55 +0000

    origin

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portefolio"> {/* Ajoutez cette ligne ici */}
       <div className="App">
         <Header />
         <Routes>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="home">
       <div className='presentation'>
-        <h2>Salut, <span className='ouz'>Je suis Ousmane</span></h2>
+        <h2>Salut 👋, <span className='ouz'>Je suis Ousmane</span></h2>
         <p>developpeur <span className='ouz'>fullstack</span></p>
         <span className='description'>Je suis un developpeur senegalais,<br/>je suis actuellement etudiant en Master<br/> a l'universite Gaston Berger<br/>de Saint-Louis </span>
         <div className='contact'>
